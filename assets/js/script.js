603d9b454e0aa70095400e61a54f457276a6ead1
Mikatep <miriamcordobacapote@gmail.com>
2022-02-21 20:45:15 +0000

    liquidity page started, header and footer copied from index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
 /** Effects with Jquery */
 
-$(document).ready(function() {
-
-$("#button-bounce").mouseover(function() {
-    doBounce($(this), 3, '10px', 300);   
-});
-
-}); 
-
-function doBounce(element, times, distance, speed) {
-    for(i = 0; i < times; i++) {
-        element.animate({marginTop: '-='+distance},speed)
-            .animate({marginTop: '+='+distance},speed);
-    }        
-}
+$("#button-bounce").mouseenter(function() {
+    doBounce($(this), 3, '20px', 300);
+  });
+  
+  
+  function doBounce(element, times, distance, speed) {
+    for (i = 0; i < times; i++) {
+      element.animate({
+          marginTop: '-=' + distance
+        }, speed)
+        .animate({
+          marginTop: '+=' + distance
+        }, speed);
+    }
+  }
 
 /** Functions ES6*/
 
