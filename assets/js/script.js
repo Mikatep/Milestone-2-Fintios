24ac142ac96b0ecaecf0710a240cb9a55d778711
Mikatep <miriamcordobacapote@gmail.com>
2022-02-23 10:32:25 +0000

    gearing ratio working

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,19 +12,29 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 })
 
+/** Variables */
+let operand1 = document.getElementById("non-curli");
+let operand2 = document.getElementById("gearing-capital");
+let operand3 = document.getElementById("gearing-reserves");
+let operand4 = document.getElementById("long-liabilities");
+
 /** gearing.html ratios */
 /** Calculates the gearing ratio in gearing.html*/
     
-const calculateGearing = (operand1, operand2, operand3, operand4,) => operand1 / (operand2 + operand3 + operand4);
+function calculateGearing() {
 
-    let gearingResult = calculateGearing();
+    a = parseInt(operand1.value)
+    b = parseInt(operand2.value)
+    c = parseInt(operand3.value)
+    d = parseInt(operand4.value)
+    document.getElementById('gearing-result').value = a / (b + c + d);
 
-/** Variables */
-let operand1 = document.getElementById("non-curli").innerText;
-let operand2 = document.getElementById("gearing-capital").innerText;
-let operand3 = document.getElementById("gearing-reserves").innerText;
-let operand4 = document.getElementById("long-liabilities").innerText;
-let
+    return false;  
+   }
+    
+
+
+
 
 
 
