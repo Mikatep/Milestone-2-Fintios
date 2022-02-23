a3dfee7dc0d8545e8f1acee8c2037228f37e9345
Mikatep <miriamcordobacapote@gmail.com>
2022-02-23 11:15:25 +0000

    ratios in gearing page javascript added and working, feedback code left

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,8 @@ let operand1 = document.getElementById("non-curli");
 let operand2 = document.getElementById("gearing-capital");
 let operand3 = document.getElementById("gearing-reserves");
 let operand4 = document.getElementById("long-liabilities");
+let operand5 = document.getElementById("cover-operating");
+let operand6 = document.getElementById("cover-payable");
 
 /** gearing.html ratios */
 /** Calculates the gearing ratio in gearing.html*/
@@ -27,9 +29,16 @@ function calculateGearing() {
     b = parseFloat(operand2.value)
     c = parseFloat(operand3.value)
     d = parseFloat(operand4.value)
-    document.getElementById('gearing-result').value = a / (b + c + d);
+    document.getElementById('gearing-result').value = a / (b + c + d);  
+   }
+    
+/** Calculates the interest cover ratio in gearing.html*/
+    
+function calculateCover() {
 
-    return false;  
+    e = parseFloat(operand5.value)
+    f = parseFloat(operand6.value)
+    document.getElementById('cover-result').value = e / f;  
    }
     
 
